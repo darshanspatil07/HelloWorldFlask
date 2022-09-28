d2675ffd08b9c9a092d6ec6bd75812df09d46488
sainath <sainathindupuri@gmail.com>
2022-09-28 18:35:30 -0500

    changes

--- hello.py
+++ hello.py
@@ -1,12 +1,9 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    return 'Congratulations! you have successfully host Flask in a Docker container!'
 
-if __name__ == '__main__':
-    app.run()
-
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0', debug = True)
